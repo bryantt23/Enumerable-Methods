3eff9766236b3d67c4f2975cca3dc7b1cb4e2028
bryant tee <bryantt23@gmail.com>
2014-12-25 02:46:07 -0800

    fin

--- enumberable_methods.rb
+++ enumberable_methods.rb
@@ -126,7 +126,7 @@ for none, it is false if there is just 1 item that yields
 =end
 
 
-
+#increment every time it yields or is true?
     def my_count
         return self.to_enum unless block_given?
         counter = 0
@@ -135,20 +135,36 @@ for none, it is false if there is just 1 item that yields
         end
         return counter
     end
+    
+=begin
+  map { |obj| block } → array 
+map → an_enumerator
+Returns a new array with the results of running block once for every element in enum.
+
+the map just takes an array and makes a new one
+i'll just think of map as make a pnew array
+
+print (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
+#proc means procedure, i guess that's the parameter
+=end
 
     def my_map(proc=nil)
+#empty array
         resp = []
+        #if there is a block & a procedure?
         if block_given? && proc
             for i in self
+                #push onto the array the procedure call for each element
                 resp.push(proc.call(yield i))
             end
             return resp
+            #if there is only a procedure given?
         elsif proc
             for i in self
                 resp.push(proc.call i)
             end
             return resp
-        else
+        else #not sure what this means
             return self.to_enum unless block_given?
             for i in self
                 resp.push(yield i)
@@ -157,8 +173,28 @@ for none, it is false if there is just 1 item that yields
         end
     end
 
+=begin http://ruby.bastardsbook.com/chapters/enumerables/
+The inject method takes a collection and reduces it to a single value, such as a sum of values:
+val = [1,3,5,7].inject(0) do |total, num|
+   total += num
+end   
+puts val   #=> 16
+
+=end
+
+=begin
+	inject {| memo, obj | block } → obj
+Combines all elements of enum by applying a binary operation, specified by a block or a symbol that names a method or operator.
+
+If you specify a block, then for each element in enum the block is passed an accumulator value (memo) and the element. If you specify a symbol instead, then each element in the collection will be passed to the named method of memo. In either case, the result becomes the new value for memo. At the end of the iteration, the final value of memo is the return value for the method.
+
+If you do not explicitly specify an initial value for memo, then uses the first element of collection is used as the initial value of memo.
+=end
+
+#i think in1 is operator, in2 is value
     def my_inject in1=nil, in2=nil
         return self.to_enum unless block_given? || in1 || in2
+        
         if in1 == nil #no input
             initial = false
             for i in self
@@ -177,6 +213,7 @@ for none, it is false if there is just 1 item that yields
                     initial = yield initial, i
                 end
             end
+            
         else #two inputs
             initial = in1
             # add case statements for treating operations, like +, -, /, *, etc
@@ -278,9 +315,6 @@ puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
 
 any? [{ |obj| block }] → true or false click to toggle source
 Passes each element of the collection to the given block. The method returns true if the block ever returns a value other than false or nil.
-=end
-@a = ["a","bit city life","c","hold on tight","e","zzzz","hit me up"]
-@b = {a: "bit city life",c: "hold on tight",p: "piratess"}
 
 puts @a.my_any? {|a| a[0] == 'j'}
 puts @a.any? {|a| a[0] == 'j'}
@@ -289,26 +323,74 @@ puts @a.any? {|a| a[0] == 'j'}
 puts @b.my_any? {|a,b| b[0] == 'h'}
 puts @b.any? {|a,b| b[0] == 'h'}
 
-=begin
+
 @a.my_none? {|a| a[0] == 'h'}
 @a.none? {|a| a[0] == 'h'}
 @b.my_none? {|a,b| a[0] == 'h'}
 @b.none? {|a,b| a[0] == 'h'}
 
-@a.my_count {|a| a[0] == 'h'}
-@a.count {|a| a[0] == 'h'}
-@b.my_count {|a,b| a[0] == 'h'}
-@b.count {|a,b| a[0] == 'h'}
+puts @a.my_count {|a| a[0] == 'h'}
+puts @a.count {|a| a[0] == 'h'}
+puts @b.my_count {|a,b| a[0] == 'h'}
+puts @b.count {|a,b| a[0] == 'h'}
+@a = ["a","bit city life","c","hold on tight","e","zzzz","hit me up"]
+@b = {a: "bit city life",c: "hold on tight",p: "piratess"}
 
 @a.my_map {|a| a[0] == 'h'}
 @a.map {|a| a[0] == 'h'}
 @b.my_map {|a,b| b[0] == 'h'}
 @b.map {|a,b| b[0] == 'h'}
 
+print (1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
+print (1..4).my_map { |i| i*i }      #=> [1, 4, 9, 16]
+
 =end
 
 def multiply_els ary
-    return ary.my_inject { |product, n| product * n }
+    return ary.my_inject { |product, n| 
+      product * n }
 end
 
-multiply_els([2,4,5])
+puts multiply_els([2,4,5])
+
+def add_els ary
+    return ary.my_inject { |sum, n| 
+      sum += n }
+end
+puts add_els([2,4,5])
+
+val = [1,3,5,7].inject(0) do |total, num|
+   total += num
+end   
+puts val   #=> 16
+
+puts (5..10).inject {|sum, n| sum + n }            #=> 45
+
+
+arr = 18.times.inject([0,1]) do |a, idx|
+    a << a[-2] + a[-1]
+end       
+
+puts arr.join(', ')
+#=> 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181
+
+
+=begin
+	Create an array of the Fibonacci sequence with inject
+
+The Fibonacci sequence consists of a sequence of integers in which each number is the sum of the previous two numbers in the sequence. By definition, the first two numbers are 0 and 1.
+
+0,1,1,2,3,5,8,13...
+
+The Fibonacci sequence is one of the most famous in mathematics as its properties have been observed in numerous fields, including the Golden Ratio used in the arts and the natural arrangement of a plant's leaves.
+
+Here's one way of creating an array with the first 20 Fibonacci numbers using a loop:
+
+
+arr = [0,1]
+18.times do
+    arr << arr[-2] + arr[-1]
+end        
+    
+Remember that negative indexes count backwards from the end of the array. So arr[-2] – when arr = [0,1] – points to the value of 0. At the end of each iteration, arr increases in length by 1.
+=end
